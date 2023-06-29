39bcd7c1be8667b53503f43d42c237935b62b244
GiaoLeXuan <giaolexuan104@gmail.com>
2023-06-29 10:15:05 +0700

    fixed warnings in code

--- src/main/java/com/example/minesweeper/EasyHighScoreController.java
+++ src/main/java/com/example/minesweeper/EasyHighScoreController.java
@@ -1,21 +1,5 @@
 package com.example.minesweeper;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.util.ResourceBundle;
-
 public class EasyHighScoreController extends RecordTableController {
 
     public EasyHighScoreController() {
--- src/main/java/com/example/minesweeper/HardHighScoreController.java
+++ src/main/java/com/example/minesweeper/HardHighScoreController.java
@@ -1,30 +1,7 @@
 package com.example.minesweeper;
 
-import javafx.collections.FXCollections;
-
-import javafx.collections.ObservableList;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.File;
-import java.net.URL;
-import java.nio.file.Files;
-import java.util.List;
-import java.util.ResourceBundle;
-
-import com.example.minesweeper.StartMenuController;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
-import javafx.scene.control.cell.PropertyValueFactory;
-
-public class HardHighScoreController extends RecordTableController{
-
-	public HardHighScoreController() {
-		super(RecordHandler.pathOfRecordFolder + "HardHighScore.txt");
-	}
+public class HardHighScoreController extends RecordTableController {
+    public HardHighScoreController() {
+        super(RecordHandler.pathOfRecordFolder + "HardHighScore.txt");
+    }
 }
